546848d6fe2df8233ffce33974e354d709b07fe8
Samray <samrayleung@gmail.com>
2016-09-16 22:29:29 +0800

    add install_useful_tool.sh file and move vim config file to a new directory named dotfile

---
+++ install_useful_tool.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Description:
+#install the tools i think are useful
+
+#top improved ,powerful than top
+echo "install htop"
+yum install htop
+
+#a code searching tool similar to "ack" ,maybe it is more familar with grep
+#,ag is with a focus on speed
+echo "install the_silver_searcher(ag)"
+yum install the_silver_searcher
