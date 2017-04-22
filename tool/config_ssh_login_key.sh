7c26628a03c063a8ca34cfd82e719037d0876db1
Samray <samrayleung@gmail.com>
2017-04-22 11:04:08 +0800

    refactor config_ssh_login_key to make it robust

--- tool/config_ssh_login_key.sh
+++ tool/config_ssh_login_key.sh
@@ -7,7 +7,7 @@ set -e
 set -x
 function config_ssh_login_key(){
     if [ $# -lt 4 ];then
-	echo "Usage: `basename $0` -u user -h hostname -p port"
+	echo "Usage: $(basename $0) -u user -h hostname -p port"
 	exit
     fi
     #if public/private key doesn't exist ,generate public/private key 
@@ -31,21 +31,15 @@ function config_ssh_login_key(){
     fi
     #check whether it is the first time to run this script and whether authorized_keys exists
     # ssh_host_and_user="$1@$2"
-    authorized_keys="~/.ssh/authorized_keys"
+    authorized_keys="$HOME/.ssh/authorized_keys"
     read -s -p "$user@$hostname's password:" password
-    if sshpass -p $password  ssh -p $port $user@$hostname test -e $authorized_keys;then
+    if sshpass -p "$password" ssh -p "$port" "$user@$hostname" test -e "$authorized_keys";then
 	echo "authorized key exists"
 	exit
     else
 	sshpass -p $password ssh  $user@$hostname -p $port "mkdir -p ~/.ssh;chmod 0700 .ssh"
 	sshpass -p $password scp -P $port  ~/.ssh/id_rsa.pub $user@$hostname:~/.ssh/authorized_keys
+	# ssh-copy-id "$user@$hostname -p $port"
     fi
 }
-function end_or_error_handler(){
-    if [ $(echo $?) = 0 ];then
-	echo "Generate successfully"
-    else
-	echo "Configure failed ,Plese check it again"
-    fi
-    exit
-}
+config_ssh_login_key "$@"
