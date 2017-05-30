caad48cde7eb02c1fefbb06589579c02cec8a06b
Samray Leung <samrayleung@gmail.com>
2017-05-30 18:18:12 +0800

    update README

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,36 +3,12 @@
 
 app_name='oh-my-unix'
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/.oh-my-unix"
-[ -z "$REPO_URI" ] && REPO_URI='git@github.com:samrayleung/Oh-my-unix.git'
-[ -z "$REPO_BRANCH" ] && REPO_BRANCH='master'
 debug_mode='0'
 fork_maintainer='0'
 
 source ./common_functions.sh
 source ./install_awesome_tool.sh
 source ./install/pip_install.sh
-
-sync_repo() {
-    local repo_path="$1"
-    local repo_uri="$2"
-    local repo_branch="$3"
-    local repo_name="$4"
-
-    msg "Trying to update $repo_name"
-
-    if [ ! -e "$repo_path" ]; then
-        mkdir -p "$repo_path"
-        git clone -b "$repo_branch" "$repo_uri" "$repo_path"
-        ret="$?"
-        success "Successfully cloned $repo_name."
-    else
-        cd "$repo_path" && git pull origin "$repo_branch"
-        ret="$?"
-        success "Successfully updated $repo_name"
-    fi
-
-    debug
-}
 
 create_symlinks() {
     local source_path="$1"
@@ -58,11 +34,6 @@ do_backup "$HOME/.dunstrc"
 do_backup "$HOME/.keysnail.js"
 do_backup "$HOME/.zshrc"
 do_backup "$HOME/.config/i3"
-
-sync_repo       "$APP_PATH" \
-                "$REPO_URI" \
-                "$REPO_BRANCH" \
-                "$app_name"
 
 create_symlinks "$APP_PATH" \
                 "$HOME"
